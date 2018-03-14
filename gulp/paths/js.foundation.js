7f865405cfbedad69153142c6f3e3adc4231102b
AndrewLeykin <katashi1328@mail.ru>
2018-03-14 16:08:34 +0300

    desktop

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -5,5 +5,8 @@ module.exports = [
   './node_modules/svg4everybody/dist/svg4everybody.legacy.min.js',
   './node_modules/remodal/dist/remodal.min.js',
   './node_modules/wowjs/dist/wow.min.js',
-  './node_modules/slick-carousel/slick/slick.min.js'
+  './node_modules/slick-carousel/slick/slick.min.js',
+  './node_modules/cleave.js/dist/cleave.min.js',
+  './node_modules/cleave.js/dist/addons/cleave-phone.ru.js',
+  './node_modules/inputmask/dist/jquery.inputmask.bundle.js'
 ];
